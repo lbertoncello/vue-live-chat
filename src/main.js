bfa4b5d2ea77c3d59868f9cf2ba974b5b54ea24e
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-04 19:01:18 -0300

    refactor: ensure the page will be loaded only when firebase is connected

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { projectAuth } from './firebase/config'
 
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+let app
+
+/*
+ * This guarantees that the app will be loaded only when the connection
+ * with Firebase is done.
+ */
+projectAuth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App)
+      .use(router)
+      .mount('#app')
+  }
+})
