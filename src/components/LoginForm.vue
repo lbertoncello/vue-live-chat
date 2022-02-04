228c1203b933b7073f9ec896a7a59909efc55c99
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-04 15:02:52 -0300

    feat: create component LoginForm

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="email" v-model="email">
+    <input type="password" required placeholder="password" v-model="password">
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    // Refs
+    const email = ref('')
+    const password = ref('')
+
+    const handleSubmit = () => {
+      console.log(email.value)
+      console.log(password.value)
+    }
+
+    return { email, password, handleSubmit }
+  }
+}
+</script>
+
+<style>
+
+</style>
