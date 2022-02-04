0c41327c0f7825a6754d8951d6c568166aad5f43
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-04 15:50:25 -0300

    feat(SignupForm): add error message in the page

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,6 +3,7 @@
     <input type="text" required placeholder="display name" v-model="displayName">
     <input type="email" required placeholder="email" v-model="email">
     <input type="password" required placeholder="password" v-model="password">
+    <div class="error">{{ error }}</div>
     <button>Sign up</button>
   </form>
 </template>
@@ -25,7 +26,7 @@ export default {
       console.log('User signed up!')
     }
 
-    return { displayName, email, password, handleSubmit }
+    return { displayName, email, password, error, handleSubmit }
   }
 }
 </script>
