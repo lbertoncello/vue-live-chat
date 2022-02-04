bb834b2ac1d7cefa7af530d0410039280441af59
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-04 15:25:03 -0300

    feat: setup firebase authentication

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyB8OAG3JP7uNBAZKS-9Ern7iEp8aeARet4",
@@ -14,7 +15,8 @@ const firebaseConfig = {
 // Init Firebase
 firebase.initializeApp(firebaseConfig)
 
+const projectAuth = firebase.auth()
 const projectFirestore = firebase.firestore()
 const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export { projectFirestore, timestamp }
+export { projectAuth, projectFirestore, timestamp }
