55cac73a96914b36a208a8fc4e6560422e71a024
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-04 17:30:02 -0300

    feat: add login handler

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,25 +2,31 @@
   <form @submit.prevent="handleSubmit">
     <input type="email" required placeholder="email" v-model="email">
     <input type="password" required placeholder="password" v-model="password">
+    <div class="error">{{ error }}</div>
     <button>Log in</button>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue'
+import useLogin from '../composables/useLogin'
 
 export default {
   setup() {
+    const { error, login } = useLogin()
+
     // Refs
     const email = ref('')
     const password = ref('')
 
-    const handleSubmit = () => {
-      console.log(email.value)
-      console.log(password.value)
+    const handleSubmit = async () => {
+      await login(email.value, password.value)
+      if (!error.value) {
+        console.log('User logged in')
+      }
     }
 
-    return { email, password, handleSubmit }
+    return { email, password, handleSubmit, error }
   }
 }
 </script>
