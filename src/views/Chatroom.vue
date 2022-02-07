96751b4b964a93b8b586b2acdd6cbccddad0665d
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-07 18:53:25 -0300

    feat(Chatroom): add component ChatWindow to the page

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow />
     <NewChatForm />
   </div>
 </template>
@@ -8,12 +9,13 @@
 <script>
 import { watch } from 'vue'
 import { useRouter } from 'vue-router'
-import Navbar from '../components/Navbar.vue'
 import getUser from '../composables/getUser'
+import Navbar from '../components/Navbar.vue'
+import ChatWindow from '../components/ChatWindow.vue'
 import NewChatForm from '../components/NewChatForm.vue'
 
 export default {
-  components: { Navbar, NewChatForm },
+  components: { Navbar, NewChatForm, ChatWindow },
   setup() {
     const router = useRouter()
     const { user } = getUser()
