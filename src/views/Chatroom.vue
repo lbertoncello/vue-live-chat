90ece62cf9e6f6f9885ff69246c3911134c74faa
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-07 14:44:26 -0300

    feat(Chatroom): add component NewChatForm to the page

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatForm />
   </div>
 </template>
 
@@ -9,9 +10,10 @@ import { watch } from 'vue'
 import { useRouter } from 'vue-router'
 import Navbar from '../components/Navbar.vue'
 import getUser from '../composables/getUser'
+import NewChatForm from '../components/NewChatForm.vue'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, NewChatForm },
   setup() {
     const router = useRouter()
     const { user } = getUser()
