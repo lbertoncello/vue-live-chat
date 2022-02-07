cfd1469fe33de772e458ed58fe9420de0f17b4a0
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-07 19:04:35 -0300

    refactor: change date format of createdAt

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <div v-if="documents" class="messages">
-      <div v-for="document in documents" :key="document.id" class="singleDocument">
-        <span class="created-at">{{ document.createdAt.toDate().toISOString() }}</span>
+    <div v-if="formattedDocuments" class="messages">
+      <div v-for="document in formattedDocuments" :key="document.id" class="singleDocument">
+        <span class="created-at">{{ document.createdAt }}</span>
         <span class="name">{{ document.name }}</span>
         <span class="message">{{ document.message }}</span>
       </div>
@@ -12,13 +12,25 @@
 </template>
 
 <script>
+import { computed } from 'vue'
 import getCollection from '../composables/getCollection'
+import { formatDistanceToNow } from 'date-fns'
 
 export default {
   setup() {
     const { error, documents } = getCollection('messages')
 
-    return { error, documents }
+    const formattedDocuments = computed(() => {
+      if (documents.value) {
+        return documents.value.map((document) => {
+          const time = formatDistanceToNow(document.createdAt.toDate())
+
+          return { ...document, createdAt: time }
+        })
+      }
+    })
+
+    return { error, documents, formattedDocuments }
   }
 }
 </script>
